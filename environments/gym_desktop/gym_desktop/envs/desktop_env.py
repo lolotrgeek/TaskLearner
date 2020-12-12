ee58477c01a1d1685f95f2aec4dd2ce93351e7ff
lolotrgeek <lolotrgeek@gmail.com>
2020-12-12 09:23:45 -0600

    optimize conditions

--- environments/gym_desktop/gym_desktop/envs/desktop_env.py
+++ environments/gym_desktop/gym_desktop/envs/desktop_env.py
@@ -234,25 +234,22 @@ class DesktopEnv(gym.Env):
         step_reward = 1
         # Actions:
         for a in action:
-            print(str(a))
-            if isinstance(a, int):
-                # integers which represent key presses
-                actions.main.key_stroke(keyMap[a])
-                # print(str(keyMap[a]))
-            elif isinstance(a, dict):
+            # print(str(a))
+            if isinstance(a, dict):
                 if "wait" in a:
                     # TODO: move to next state but, wait x amount before taking next action...
                     # time.sleep(a["wait"])
-                    print('wait...')
+                    return self.state, step_reward, done, {}
+            elif isinstance(a, int):
+                # integers which represent key presses
+                actions.main.key_stroke(keyMap[a])
+                # print(str(keyMap[a]))
                     
             elif isinstance(a, object):
                 # objects which represent x,y coordinate with a buttonmask (clicks)
                 # TODO: decode/test actual mouse movements
                 actions.main.mouse_action(a)
                 # print(str(a.x), ',', str(a.y))
-
-            else:
-                print('no action')
 
         if self.last_time - self.start_time > self.time_limit:
             print("Ending...")
