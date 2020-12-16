43b465775ee83afcaee80838b557a771188782b8
lolotrgeek <lolotrgeek@gmail.com>
2020-12-16 13:52:53 -0600

    add scaling and codec

--- environments/gym_desktop/gym_desktop/envs/CaptureStream.py
+++ environments/gym_desktop/gym_desktop/envs/CaptureStream.py
@@ -3,10 +3,15 @@ from threading import Thread
 import cv2
 
 class WebcamVideoStream:
-	def __init__(self, src=0, name="WebcamVideoStream"):
+	def __init__(self, src=0, name="WebcamVideoStream", width=800, height=640):
 		# initialize the video camera stream and read the first frame
 		# from the stream
 		self.stream = cv2.VideoCapture(src)
+		self.codec = 0x47504A4D  # MJPG
+		# self.camera.set(cv2.CAP_PROP_FPS, 30.0)
+		self.stream.set(cv2.CAP_PROP_FOURCC, self.codec)
+		self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+		self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 		(self.grabbed, self.frame) = self.stream.read()
 
 		# initialize the thread name
