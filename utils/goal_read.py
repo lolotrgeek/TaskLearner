eb0d3f1a78f38565381faa561256b97067c9cd81
lolotrgeek <lolotrgeek@gmail.com>
2020-12-23 08:38:15 -0600

    test sum of goal

--- utils/goal_read.py
+++ utils/goal_read.py
@@ -2,8 +2,11 @@
 
 import pickle
 import cv2
+import numpy as np
 
+# TODO NEXT move this into env and get current state distance from goal, then use as a reward signal 
 with open('goal.state', 'rb') as filehandle:
     goal = pickle.load(filehandle)
     cv2.imshow("Goal", goal)
-    cv2.waitKey(0)	
+    cv2.waitKey(0)
+    print(np.sum(goal))
