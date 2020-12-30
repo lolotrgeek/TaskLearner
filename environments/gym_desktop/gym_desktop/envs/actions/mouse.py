3d1f6da4c2e0912c12e4477ead09a87df25c1ebd
lolotrgeek <lolotrgeek@gmail.com>
2020-12-30 10:37:35 -0600

    add absolute mouse

--- environments/gym_desktop/gym_desktop/envs/actions/mouse.py
+++ environments/gym_desktop/gym_desktop/envs/actions/mouse.py
@@ -1,14 +1,20 @@
-# Mouse events use a relative coordinate system. 
-# When recorded the x,y values are the distance from the last coord divided by height for x and width for y
-# This allows the system to adapt to changing screen sizes 
 # Reference:
 # https://github.com/mtlynch/tinypilot/commit/ea853b54fa56ce275b6f776caa5a839451013e84#diff-d589676c00c019dfd703ad4aac7f458ffe5efda9116a930d3474d28aa3def657
 # https://www.raspberrypi.org/forums/viewtopic.php?t=234495
 # https://wiki.osdev.org/Mouse_Input
 from . import write as hid_write
 
-def send_mouse_event(mouse_path, buttons, relative_x, relative_y,
+def send_mouse_event_rel(mouse_path, buttons, relative_x, relative_y,
                      vertical_wheel_delta, horizontal_wheel_delta):
+    '''
+    NOTE: requires a HID descriptor that supports bitshifted x , y inputs
+
+    Scale mouse events with a relative coordinate system.
+
+    When recorded the x,y values are the distance from the last coord divided by height for x and width for y
+
+    This allows the system to adapt to changing screen sizes and convert pixel values to movements
+    '''
     x, y = _scale_mouse_coordinates(relative_x, relative_y)
 
     buf = [0] * 7
@@ -24,7 +30,6 @@ def send_mouse_event(mouse_path, buttons, relative_x, relative_y,
     # 1. pre-scaling coords on record
     # 2. writing bytes explicitly without constructing a bytearray
 
-    # hid_write.write_to_hid_interface(mouse_path, buf)
     hid_write._write_to_hid_interface_immediately(mouse_path, buf)
 
 
@@ -34,3 +39,7 @@ def _scale_mouse_coordinates(relative_x, relative_y):
     x = int(relative_x * max_hid_value)
     y = int(relative_y * max_hid_value)
     return x, y
+
+def send_mouse_event(mouse_path, button, dx, dy, wheel):
+    report = [button, dx & 0xff, dy & 0xff, wheel & 0xff]
+    hid_write._write_to_hid_interface_immediately(mouse_path, report)
