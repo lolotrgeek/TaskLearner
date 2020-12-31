c57017f224e801bb6c824b4ab8ea726047428344
lolotrgeek <lolotrgeek@gmail.com>
2020-12-31 07:53:57 -0600

    immediate key strokes

--- environments/gym_desktop/gym_desktop/envs/actions/keyboard.py
+++ environments/gym_desktop/gym_desktop/envs/actions/keyboard.py
@@ -24,7 +24,7 @@ def send_keystroke(keyboard_path, control_keys, hid_keycode):
     buf = [0] * 8
     buf[0] = control_keys
     buf[2] = hid_keycode
-    hid_write.write_to_hid_interface(keyboard_path, buf)
+    hid_write._write_to_hid_interface_immediately(keyboard_path, buf)
 
     # If it's not a modifier keycode, add a message indicating that the key
     # should be released after it is sent.
@@ -33,4 +33,4 @@ def send_keystroke(keyboard_path, control_keys, hid_keycode):
 
 
 def release_keys(keyboard_path):
-    hid_write.write_to_hid_interface(keyboard_path, [0] * 8)
+    hid_write._write_to_hid_interface_immediately(keyboard_path, [0] * 8)
