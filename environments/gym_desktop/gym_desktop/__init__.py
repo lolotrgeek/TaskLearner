93f76f5dfa942ab3088f5291eee185d3cf1ca707
lolotrgeek <lolotrgeek@gmail.com>
2021-01-16 11:34:48 -0600

    remove desktop env

--- environments/gym_desktop/gym_desktop/__init__.py
+++
@@ -1,6 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='Desktop-v0',
-    entry_point='gym_desktop.envs:DesktopEnv',
-)
--- environments/gym_desktop/gym_desktop/envs/__init__.py
+++
@@ -1,3 +0,0 @@
-from gym_desktop.envs.desktop_env import DesktopEnv
-from . import actions
-from . import CaptureStream
