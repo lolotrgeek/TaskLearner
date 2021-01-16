3c80a10e9719d0328871b86468fbbea977aa64f2
lolotrgeek <lolotrgeek@gmail.com>
2021-01-16 11:30:48 -0600

    remove multi

--- environments/gym_multi/gym_multi/__init__.py
+++
@@ -1,6 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='Multi-v0',
-    entry_point='gym_multi.envs:MultiEnv',
-)
--- environments/gym_multi/gym_multi/envs/__init__.py
+++
@@ -1,2 +0,0 @@
-from gym_multi.envs.multi_env import MultiEnv
-from gym_multi.envs.monitor import Monitor
